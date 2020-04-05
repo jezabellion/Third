094d7ed2682d582f7b5fc8610c9cb90e29a04495
Jezabellion <jeremy.lefebre@gmail.com>
2020-04-05 11:09:07 +0200

    Bottom nav styled

--- src/routes/base.svelte
+++ src/routes/base.svelte
@@ -5,6 +5,63 @@
 <style media="screen" lang="scss">
 	@import "./style/theme.scss";
 
+  .hero {
+    background-color: pink;
+  }
+
+  .hero-body h1,
+  .hero-body h2 {
+    color: white;
+  }
+
+
+  .hero-body h1 {
+  font-family: 'Engagement';
+  font-size: 150px;
+  color: #FFFFFF;
+  letter-spacing: -6.25px;
+  text-align: center;
+  margin-top: -70px;
+  font-weight: normal;
+  }
+
+  .hero-body h1 {
+    margin-top: auto;
+  }
+
+  .hero-body {
+    padding-top: 8rem;
+    padding-bottom: 8rem;
+  }
+
+  .hero-image {
+    margin-left: 8rem;
+    margin-right: 8rem;
+  }
+
+  .hero-foot {
+    margin-bottom: 2rem;
+  }
+
+  .hero .tabs ul,
+  .hero .hero-foot .tabs ul, {
+    padding-left: 14rem;
+    padding-right: 14rem;
+  }
+
+  .hero .hero-foot .tabs ul {
+    padding-top: 3rem;
+  }
+
+  .hero-foot .hero-body h1 {
+    margin-top: auto;
+  }
+
+  .hero-foot .hero-body {
+      padding-top: 8rem;
+      padding-bottom: 8rem;
+  }
+
   .shoes,
   .home-pic {
     background-image: url("../images/shoes.jpg") !important;
@@ -12,7 +69,27 @@
     background-position: center;
     margin-left: 8rem;
     margin-right: 8rem;
+  }
 
+
+  // bottom nav
+  .hero .hero-foot .tabs a {
+    color: $black;
+    font-size: 1rem;
+    font-weight: bold;
+  }
+
+  .hero .hero-foot .tabs a:hover {
+    color: $grey;
+  }
+
+  .chapter-close h3 {
+    color: $grey;
+    text-align: center;
+    font-family: 'Engagement';
+    font-size: 4rem;
+    line-height: 4rem;
+    margin: 10rem 5rem;
   }
 
 </style>
@@ -43,23 +120,22 @@
     <div class="shoes hero-body">
       <div class="container has-text-centered">
         <h1 class="title">
-          {destination}
+          Wanderlust
         </h1>
       </div>
     </div>
   </div>
 
   <!-- Hero footer: will stick at the bottom -->
-  <div class="hero-foot">
-    <nav class="tabs is-boxed is-fullwidth">
+  <div class="hero-foot is-hidden-mobile">
+    <nav class="tabs is-fullwidth">
       <div class="container">
         <ul>
-          <li class="is-active"><a>Overview</a></li>
-          <li><a>Modifiers</a></li>
-          <li><a>Grid</a></li>
-          <li><a>Elements</a></li>
-          <li><a>Components</a></li>
-          <li><a>Layout</a></li>
+          <li class="is-active"><a>Home</a></li>
+          <li><a>Morocco</a></li>
+          <li><a>Istanbul</a></li>
+          <li><a>Russia</a></li>
+          <li><a>Peru</a></li>
         </ul>
       </div>
     </nav>
