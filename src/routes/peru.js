fa52858cee7be506c14cf29bb73e94d920d1e27d
Jezabellion <jeremy.lefebre@gmail.com>
2020-04-11 18:43:13 +0200

    Added Peru JS

---
+++ src/routes/peru.js
@@ -0,0 +1,65 @@
+// Travel Page Data
+// Peru
+
+export const copy = {
+  blurb: 'Next level is a big next step in the propsition New new new shoes mane man to take your love of astronomy to the',
+  travelers: 'Izzy & Jeremy',
+  farewell: 'Merci beaucoup Morocco!'
+}
+
+export const chapter = {
+      // one: 'Aït Benhaddou',
+      // two: 'Erg Chebbi'
+    }
+
+export const photos = {
+  // Central Restaurant
+   one: [
+      { src: 'images/peru/IMG_4F7880AC67C8-1.jpeg', alt: 'yup' },
+      { src: 'images/peru/IMG_4F7880AC67C8-2.jpeg', alt: 'yup' },
+      { src: 'images/peru/IMG_4F7880AC67C8-3.jpeg', alt: 'yup' },
+      { src: 'images/peru/IMG_4F7880AC67C8-4.jpeg', alt: 'yup' },
+      { src: 'images/peru/IMG_4F7880AC67C8-5.jpeg', alt: 'yup' },
+      { src: 'images/peru/IMG_4F7880AC67C8-6.jpeg', alt: 'yup' },
+      { src: 'images/peru/IMG_4F7880AC67C8-7.jpeg', alt: 'yup' },
+      { src: 'images/peru/IMG_4F7880AC67C8-8.jpeg', alt: 'yup' }
+    ],
+    // Cuzco
+    two: [
+       { src: 'images/peru/IMG_CA62B005A47D-1.jpeg', alt: 'yup' },
+       { src: 'images/peru/IMG_CA62B005A47D-2.jpeg', alt: 'yup' },
+       { src: 'images/peru/IMG_CA62B005A47D-3.jpeg', alt: 'yup' }
+     ],
+     // Moutain
+     three: [
+        { src: 'images/peru/IMG_CA62B005A47D-1.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-2.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-3.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-4.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-5.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-6.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-7.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-8.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-9.jpeg', alt: 'yup' },
+        { src: 'images/peru/IMG_CA62B005A47D-10.jpeg', alt: 'yup' }
+      ],
+      // Wedding
+      four: [
+         { src: 'images/peru/IMG_73848FD5B2F4-1.jpeg', alt: 'yup' },
+         { src: 'images/peru/IMG_73848FD5B2F4-2.jpeg', alt: 'yup' },
+         { src: 'images/peru/IMG_73848FD5B2F4-3.jpeg', alt: 'yup' },
+         { src: 'images/peru/IMG_73848FD5B2F4-4.jpeg', alt: 'yup' },
+         { src: 'images/peru/IMG_73848FD5B2F4-5.jpeg', alt: 'yup' },
+         { src: 'images/peru/IMG_73848FD5B2F4-6.jpeg', alt: 'yup' },
+         { src: 'images/peru/IMG_73848FD5B2F4-7.jpeg', alt: 'yup' },
+         { src: 'images/peru/IMG_73848FD5B2F4-8.jpeg', alt: 'yup' },
+         { src: 'images/peru/IMG_73848FD5B2F4-9.jpeg', alt: 'yup' }
+       ],
+       // Spilt screen
+       four: [
+          { src: 'images/peru/IMG_A9068D7FA463-1.jpeg', alt: 'yup' },
+          { src: 'images/peru/IMG_A9068D7FA463-1.jpeg', alt: 'yup' },
+          { src: 'images/peru/IMG_A9068D7FA463-1.jpeg', alt: 'yup' },
+          { src: 'images/peru/IMG_A9068D7FA463-1.jpeg', alt: 'yup' }
+        ]
+      };
